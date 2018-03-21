f135031e843766e26eab08660df084d415d2e8ed
Daniel Carneiro <danielggcarneiro7@gmail.com>
2018-03-21 15:30:34 +0000

    teste

--- app/src/main/java/com/example/paulo/attendly/MainActivity.java
+++ app/src/main/java/com/example/paulo/attendly/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.paulo.attendly;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.nfc.Tag;
+import android.os.Environment;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +18,10 @@ import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 
+import com.example.paulo.attendly.firebase.connectFirebase;
+
+import java.io.File;
+import java.io.FilenameFilter;
 import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity implements BeaconConsumer {
@@ -24,13 +32,17 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        beaconManager = BeaconManager.getInstanceForApplication(this);
-        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
-        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
-         beaconManager.getBeaconParsers().add(new BeaconParser().
-                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
-        beaconManager.bind(this);
+//        beaconManager = BeaconManager.getInstanceForApplication(this);
+//        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
+//        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
+//         beaconManager.getBeaconParsers().add(new BeaconParser().
+//                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+//        beaconManager.bind(this);
+        loadFileList();
+        connectFirebase.writeData();
+        connectFirebase.readData();
     }
+
 
     @Override
     protected void onDestroy() {
@@ -66,15 +78,69 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         beaconManager.addRangeNotifier(new RangeNotifier() {
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                Log.d(TAG,"BEACONS"+beacons);
+                Log.d(TAG, "BEACONS" + beacons);
                 if (beacons.size() > 0) {
-                    Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+                    Log.i(TAG, "The first beacon I see is about " + beacons.iterator().next().getDistance() + " meters away.");
                 }
             }
         });
 
         try {
             beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
-        } catch (RemoteException e) {    }
+        } catch (RemoteException e) {
+        }
+    }
+
+
+    private String[] mFileList;
+    private File mPath = new File(Environment.getExternalStorageDirectory() + "//yourdir//");
+    private String mChosenFile;
+    private static final String FTYPE = ".txt";
+    private static final int DIALOG_LOAD_FILE = 1000;
+
+    private void loadFileList() {
+        try {
+            mPath.mkdirs();
+        } catch (SecurityException e) {
+            Log.e(TAG, "unable to write on the sd card " + e.toString());
+        }
+        if (mPath.exists()) {
+            FilenameFilter filter = new FilenameFilter() {
+
+                @Override
+                public boolean accept(File dir, String filename) {
+                    File sel = new File(dir, filename);
+                    return filename.contains(FTYPE) || sel.isDirectory();
+                }
+
+            };
+            mFileList = mPath.list(filter);
+        } else {
+            mFileList = new String[0];
+        }
+    }
+
+    protected Dialog onCreateDialog(int id) {
+        Dialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        switch (id) {
+            case DIALOG_LOAD_FILE:
+                builder.setTitle("Choose your file");
+                if (mFileList == null) {
+                    Log.e(TAG, "Showing file picker before loading the file list");
+                    dialog = builder.create();
+                    return dialog;
+                }
+                builder.setItems(mFileList, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        mChosenFile = mFileList[which];
+                        //you can do stuff with the file here too
+                    }
+                });
+                break;
+        }
+        dialog = builder.show();
+        return dialog;
     }
 }
