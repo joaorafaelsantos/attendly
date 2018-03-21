6c181192ffe83f880ad64039e1c5540b10c78c09
João Santos <9150539@esmad.ipp.pt>
2018-03-21 15:28:59 +0000

    Revert "Revert "Models (POJO's)""
    This reverts commit e6b3ddcd3b444fd6ab1b5e92695b4e2364720abb.

---
+++ app/src/main/java/com/example/paulo/attendly/model/Classroom.java
@@ -0,0 +1,41 @@
+package com.example.paulo.attendly.model;
+
+/**
+ * Created by JOAO on 21/03/2018.
+ */
+
+public class Classroom {
+    private int id;
+    private String name;
+    private String id_beacon;
+
+    public Classroom(int id, String name, String id_beacon) {
+        this.id = id;
+        this.name = name;
+        this.id_beacon = id_beacon;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId_beacon() {
+        return id_beacon;
+    }
+
+    public void setId_beacon(String id_beacon) {
+        this.id_beacon = id_beacon;
+    }
+}
