7e706021e11d7a368bd997bfbdb1fe1f1c74f420
Daniel Carneiro <danielggcarneiro7@gmail.com>
2018-03-21 16:19:13 +0000

    test 3

--- app/src/main/java/com/example/paulo/attendly/MainActivity.java
+++ app/src/main/java/com/example/paulo/attendly/MainActivity.java
@@ -42,7 +42,8 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
 //         beaconManager.getBeaconParsers().add(new BeaconParser().
 //                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
 //        beaconManager.bind(this);
-        loadFileList();
+
+        
 //        connectFirebase.writeData();
 //        connectFirebase.readData();
     }
@@ -112,55 +113,5 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     }
 
 
-    private String[] mFileList;
-    private File mPath = new File(Environment.getExternalStorageDirectory() + "//yourdir//");
-    private String mChosenFile;
-    private static final String FTYPE = ".txt";
-    private static final int DIALOG_LOAD_FILE = 1000;
 
-    private void loadFileList() {
-        try {
-            mPath.mkdirs();
-        } catch (SecurityException e) {
-            Log.e(TAG, "unable to write on the sd card " + e.toString());
-        }
-        if (mPath.exists()) {
-            FilenameFilter filter = new FilenameFilter() {
-
-                @Override
-                public boolean accept(File dir, String filename) {
-                    File sel = new File(dir, filename);
-                    return filename.contains(FTYPE) || sel.isDirectory();
-                }
-
-            };
-            mFileList = mPath.list(filter);
-        } else {
-            mFileList = new String[0];
-        }
-    }
-
-    protected Dialog onCreateDialog(int id) {
-        Dialog dialog = null;
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-        switch (id) {
-            case DIALOG_LOAD_FILE:
-                builder.setTitle("Choose your file");
-                if (mFileList == null) {
-                    Log.e(TAG, "Showing file picker before loading the file list");
-                    dialog = builder.create();
-                    return dialog;
-                }
-                builder.setItems(mFileList, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        mChosenFile = mFileList[which];
-                        //you can do stuff with the file here too
-                    }
-                });
-                break;
-        }
-        dialog = builder.show();
-        return dialog;
-    }
 }
