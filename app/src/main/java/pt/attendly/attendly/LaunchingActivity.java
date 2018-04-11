ce736fee40b54ee58f9940fdc781ca85f66fb514
Daniel Carneiro <danielggcarneiro7@gmail.com>
2018-04-11 15:07:50 +0100

    launching done

--- app/src/main/java/pt/attendly/attendly/LaunchingActivity.java
+++ app/src/main/java/pt/attendly/attendly/LaunchingActivity.java
@@ -1,10 +1,13 @@
 package pt.attendly.attendly;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,31 +17,51 @@ import pt.attendly.attendly.other.userPref;
 
 public class LaunchingActivity extends AppCompatActivity {
 
+    ImageView ivLaunching;
+    Intent intent;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+
         setContentView(R.layout.activity_launching);
 
+        ivLaunching = findViewById(R.id.ivLaunching);
+
+        AnimationDrawable loading_animation = (AnimationDrawable) ivLaunching.getDrawable();
+        loading_animation.start();
+        cdt.start();
         User currentUser = userPref.loadUserInfo(this);
-        if(currentUser == null)
-        {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        }
-        else
-        {
+        if (currentUser == null) {
+            intent = new Intent(this, LoginActivity.class);
+
+        } else {
 
             LoginActivity.loggedUser = currentUser;
             ProfileActivity.userImage = userPref.loadUserImage(this);
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+            intent = new Intent(this, MainActivity.class);
         }
 
-
-
-
-
-
     }
+
+    CountDownTimer cdt = new CountDownTimer(2000, 2000) {
+        @Override
+        public void onTick(long l) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            startActivity(intent);
+        }
+    };
 
 }
